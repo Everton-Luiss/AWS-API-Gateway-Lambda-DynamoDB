0566cf0f305280df4f64c80592e2653a6cd13e87
Everton-Luiss <elssufpa@gmail.com>
2021-03-24 14:24:26 -0300

    Update create.py

--- create.py
+++ create.py
@@ -5,37 +5,42 @@ from datetime import datetime
 import os
 
 def lambda_handler(event, context):
-    # Instanciating connection objects with DynamoDB using boto3 dependency
     dynamodb = boto3.resource('dynamodb')
     client = boto3.client('dynamodb')
 
-    # Getting the table the table Temperatures object
-    tableTemperature = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
 
-    # Getting the current datetime and transforming it to string in the format bellow
     eventDateTime = (datetime.now()).strftime("%Y-%m-%d %H:%M:%S")
     todo_id = str(uuid.uuid1())
-    #deviceId = event['deviceId']
     text = event['text']
 
-    # Putting a try/catch to log to user when some error occurs
-    try:
+     if 'text' not in event:
+        logging.error('Validation Failed')
+        return {'statusCode': 422,
+                'body': json.dumps({'error_message': 'Couldn\'t create the todo item. Insert a text key.'})}
 
-        tableTemperature.put_item(
-            Item={
-                'eventDateTime': eventDateTime,
-                'todo_id': todo_id,
-                'text': text
+    if not event['text']:
+        logging.error('Validation Failed - text was empty. %s', event)
+        return {'statusCode': 422,
+                'body': json.dumps({'error_message': 'Couldn\'t create the todo item. As text was empty.'})}
+    else:
+        try:
+
+            table.put_item(
+                Item={
+                    'eventDateTime': eventDateTime,
+                    'todo_id': todo_id,
+                    'text': text
+                }
+            )
+
+            return {
+                'statusCode': 200,
+                'body': json.dumps('Succesfully inserted temperature!')
             }
-        )
-
-        return {
-            'statusCode': 200,
-            'body': json.dumps('Succesfully inserted temperature!')
-        }
-    except:
-        print('Closing lambda function')
-        return {
-            'statusCode': 400,
-            'body': json.dumps('Error saving the temperature')
-        }
+        except:
+            print('Closing lambda function')
+            return {
+                'statusCode': 400,
+                'body': json.dumps('Error saving text')
+            }
