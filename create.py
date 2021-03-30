74effcb9dc90f68030681b0b505fca4f29cb64f4
Everton-Luiss <elssufpa@gmail.com>
2021-03-30 15:05:14 -0300

    Update create.py

--- create.py
+++ create.py
@@ -1,7 +1,8 @@
-import json
-import boto3
-import uuid
 from datetime import datetime
+import logging
+import boto3
+import json
+import uuid
 import os
 
 def lambda_handler(event, context):
@@ -14,18 +15,18 @@ def lambda_handler(event, context):
     todo_id = str(uuid.uuid1())
     text = event['text']
 
-     if 'text' not in event:
+    if 'text' not in text:
         logging.error('Validation Failed')
+
         return {'statusCode': 422,
                 'body': json.dumps({'error_message': 'Couldn\'t create the todo item. Insert a text key.'})}
 
-    if not event['text']:
+    elif not event['text']:
         logging.error('Validation Failed - text was empty. %s', event)
         return {'statusCode': 422,
                 'body': json.dumps({'error_message': 'Couldn\'t create the todo item. As text was empty.'})}
     else:
         try:
-
             table.put_item(
                 Item={
                     'eventDateTime': eventDateTime,
